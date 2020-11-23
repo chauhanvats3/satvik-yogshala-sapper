9dc814256726d61252edf18ad3bbaa63340387af
Vatsal <chauhan.vats3@gmail.com>
2020-11-23 19:53:55 +0530

    created gallery

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -1,11 +1,181 @@
 <script>
 
+    var images = [
+        {
+            src: "images/flower.jpg",
+            span_row: "3",
+            span_col: "3"
+        },
+        {
+            src: "images/food1.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/team/pritam-singh-rawat.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/food2.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/pritam_namaskar.jpg",
+            span_row: "3",
+            span_col: "3"
+        },
+        {
+            src: "images/spa.jpg",
+            span_row: "4",
+            span_col: "4"
+        },
+        {
+            src: "images/travel1.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/travel2.jpg",
+            span_row: "3",
+            span_col: "3"
+        },
+        {
+            src: "images/team/yogi-vipulraj.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/yoga1.jpg",
+            span_row: "4",
+            span_col: "4"
+        },
+        {
+            src: "images/yoga2.jpg",
+            span_row: "3",
+            span_col: "3"
+        },
+        {
+            src: "images/yoga3.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/yoga4.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/yoga5.jpg",
+            span_row: "2",
+            span_col: "2"
+        },
+        {
+            src: "images/team/yogi-pankaj-paswan.jpg",
+            span_row: "1",
+            span_col: "1"
+        },
+        {
+            src: "images/yoga6.jpg",
+            span_row: "2",
+            span_col: "3"
+        },
+    ];
+
+
+    var col1 = [];
+    var col2 = [];
+    var col3 = [];
+    var col4 = [];
+    images.forEach(function (value, i) {
+        var app_ind = i % 4;
+        if (app_ind == 0) {
+            col1.push(value);
+        } else if (app_ind == 1) {
+            col2.push(value);
+        } else if (app_ind == 2) {
+            col3.push(value);
+        } else if (app_ind == 3) {
+            col4.push(value);
+        }
+    });
 </script>
 
 <style>
+    .row {
+        display: -ms-flexbox;
+        /* IE10 */
+        display: flex;
+        -ms-flex-wrap: wrap;
+        /* IE10 */
+        flex-wrap: wrap;
+        align-items: flex-start;
+        padding: 0 4px;
+        margin-bottom: 20px;
+    }
 
+    /* Create four equal columns that sits next to each other */
+    .column {
+        -ms-flex: 25%;
+        /* IE10 */
+        flex: 25%;
+        max-width: 25%;
+    }
+
+    .image_wrap {
+        vertical-align: middle;
+        width: 100%;
+        cursor: pointer;
+        overflow: hidden;
+        margin: 5px;
+    }
+
+    .image_wrap img {
+        width: 100%;
+        transition: 0.3s all ease-in-out;
+    }
+
+    .image_wrap:hover img {
+        transform: scale(1.2);
+    }
+
+
+    /* Responsive layout - makes a two column-layout instead of four columns */
+    @media screen and (max-width: 800px) {
+        .column {
+            flex: 50%;
+            max-width: 50%;
+        }
+    }
+
+    /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 600px) {
+        .column {
+            flex: 100%;
+            max-width: 100%;
+        }
+    }
 </style>
 
 <div class="gallery">
-    GALLERY
+    <div class="row">
+        <div class="column">
+            {#each col1 as img}
+            <div class="image_wrap"><img src="{img.src}" alt=""></div>            {/each}
+        </div>
+        <div class="column"> 
+            {#each col2 as img}
+            <div class="image_wrap"><img src="{img.src}" alt=""></div>
+        {/each}
+    </div>
+        <div class="column"> 
+            {#each col3 as img}
+            <div class="image_wrap"><img src="{img.src}" alt=""></div>        {/each}
+    </div>
+        <div class="column"> 
+            {#each col4 as img}
+            <div class="image_wrap"><img src="{img.src}" alt=""></div>        {/each}
+    </div>
+    </div>
 </div>
--- src/routes/yoga-gurus.svelte
+++ src/routes/yoga-gurus.svelte
@@ -1,23 +1,23 @@
 <script>
     import Card_Horizontal from '../components/card_horizontal.svelte';
     let teachers = [{
-        image: "images_old/team/pritam-singh-rawat.jpg",
+        image: "images/team/pritam-singh-rawat.jpg",
         title: "Pritam Rawat",
         desc: `Yogi Pritam, manager and Hatha Yoga Instructor of Anand Yog Rishikesh with considerable experience of teaching and practicing yoga is a gentle soul. His sheer inclination towards spirituality and absolute precision in teaching creates an ambiance of harmony and love.Pritam is also a practitioner of acupressure since he acquired the knowledge and certification long ago. Due to his knowledge of acupressure other healing sciences and under the guidance of masters, his approach is basically therapy based. He is also versed in Ashtanga Vinyasa Yoga`
     },
     {
-        image: "images_old/team/swami-manasananda.jpg",
+        image: "images/team/swami-manasananda.jpg",
         title: "Swami Manasanada",
         desc: `Gives beautiful Yoga training in Sivananda style, Hatha Yoga for both beginners and the advanced. He studied Yoga T.T.C, A.T.T.C and Sadhana Intensive courses at International Sivananda Yoga and Vedanta Centre at Neyyar Dam in Kerala from 2000 to 2002 and holds his master’s degree from the PSK College of Yoga Science and now he is Philosophy Teacher in Anand Yogshala.He has a 15-year long curriculum of studies in different yogic disciplines like the ancient Vedanta philosophy, the Upanishads, Bhagavad Gita and Self-knowledge/Tatwa Bodha in prestigious ashram schools, namely Chinmaya Mission, the Arsha Vidya Gurukulam and the Self Knowledge Foundation at Tiruvanmalai under renowned Masters like Swami Dayananda and Swami Suddhananda respectively. <br/>
         His teachings also include: South India style Vedic Mantra Chanting classes. Patanjali Yoga Sutra Lecture on 8 limbs. Self Knowledge Lecture. Relativity towards Reality Lectures. Unconditional Love with Awareness towards Self Lecture. Indian Philosophy and Spiritual Satsang classes. Manash Ranjan spent years with different spiritual masters in the different places for the sake of truth, because he has always been a seeker of truth since his childhood. .He has also started his intense spiritual journey and some unique spiritual practices. He says Yoga is his life and he shares his experience and whatever he has learned through this miraculous journey of yoga.`
 
     }, {
-        image: "images_old/team/yogi-pankaj-paswan.jpg",
+        image: "images/team/yogi-pankaj-paswan.jpg",
         title: "Yogi Pankaj Paswan",
         desc: `Pankaj Paswan is a certified yoga teacher possesses a Bachelor degree of arts in yoga & Naturopathy from Uttarakhand Open University, India. He is also certified by International Institute of Yoga and Meditation from Ved Niketan Ashram, Ram Jhula, Rishikesh, India. Currently, he is teaching Pranayama, Yoga Therapy and Meditation.Pankaj Paswan has made a number of people happy throughout the World with his Teaching and Therapy Practice.`
 
     }, {
-        image: "images_old/team/yogi-vipulraj.jpg",
+        image: "images/team/yogi-vipulraj.jpg",
         title: "Yogi Vipulraj",
         desc: `Yogi Vipulraj was born in the northern Indian traditional Aryan family. According to the tradition of his family, he started the spiritual and physical search of yoga under the guidance of his parents. Vipul holds a doctor degree in yoga science. He has two master degrees, being one in yoga philosophy and other in Sanskrit, the ancient language of yoga.He has also acquired a postgraduate diploma in yoga and holistic therapeutic techniques. Many of his research documents on Yoga Culture have been published in some famous yoga journals. Vipul has been teaching yoga asana, Sanskrit, and philosophy for many years. His methodology focuses on panic energy healing for opening psychic channels and mental peace. <br/>
 He teaches with a style of scientific approach to ensure a proper alignment in every posture. His classes are taught in a conscious balance of strength and relaxation to create a loving and peaceful environment. Vipul is always passionate about sharing his understanding and vast experience of the traditional Himalayan yoga, which he has inherited through his parents from a lineage of the ancient yogis.`
